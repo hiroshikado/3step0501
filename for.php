06bba6597dbe9dcc001489c698b29192116a8097
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-10-31 19:54:35 +0900

    see-for

---
+++ for.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8" >
+	<title>forを使ってみよう</title>
+	<link rel="stylesheet" href="/php/css/skyblue.css">
+</head>
+<body>
+	<div class="bg-success padding-y-20">
+		<div class="container text-center">
+			<h1>forを使ってみよう</h1>
+		</div>
+	</div>
+	<div class="container padding-y-20">
+		<!-- ここから -->
+		<!-- ここまで -->
+		<ul>
+			<!-- ここから -->
+			<!-- ここまで -->
+		</ul>
+	</div>
+</body>
+</html>
---
+++ for_comp.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8" >
+	<title>forを使ってみよう</title>
+	<link rel="stylesheet" href="/php/css/skyblue.css">
+</head>
+<body>
+	<div class="bg-success padding-y-20">
+		<div class="container text-center">
+			<h1>forを使ってみよう</h1>
+		</div>
+	</div>
+	<div class="container padding-y-20">
+		<?php
+		for ( $i = 1; $i <= 5; $i++ ) {
+			echo "カウンタ変数：". $i. "<br>";
+		}
+		?>
+		<ul>
+			<?php
+			for ( $i = 0; $i < 5; $i++ ) {
+				echo "<li>";
+				echo $i;
+				echo "</li>";
+			}
+			?>
+		</ul>
+	</div>
+</body>
+</html>
